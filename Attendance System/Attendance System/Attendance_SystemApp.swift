b2c1be7d18700b2ed7c1a43ec427e4d192ee3712
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2025-01-21 15:38:57 +0100

    added

---
+++ Attendance System/Attendance System/Attendance_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Attendance_SystemApp.swift
+//  Attendance System
+//
+//  Created by Mac on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Attendance_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
